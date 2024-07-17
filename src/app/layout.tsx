02de9c6f063348f9da0cbdd03a153961905691d9
Saiyam Dubey <saiyamdubey8787@gmail.com>
2024-07-18 02:05:40 +0530

    Changes the context

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,39 +1,39 @@
-import './globals.css';
-import type { Metadata } from 'next';
-import SideBar from '@/components/Sidebar';
-import Navbar from '@/components/Navbar';
-import ContactForm from '@/components/ContactForm';
-import { ContactFormProvider, MenuProvider } from '@/components/Provider';
-import { Toaster } from 'react-hot-toast';
-import { Analytics } from "@vercel/analytics/react"
+import "./globals.css";
+import type { Metadata } from "next";
+import SideBar from "@/components/Sidebar";
+import Navbar from "@/components/Navbar";
+import ContactForm from "@/components/ContactForm";
+import { ContactFormProvider, MenuProvider } from "@/components/Provider";
+import { Toaster } from "react-hot-toast";
+import { Analytics } from "@vercel/analytics/react";
 export const metadata: Metadata = {
-    title: 'Ez Snippet',
-    description: 'Learning made EZ',
+  title: "Saiyam Dubey",
+  description: "Loved by Me",
 };
 
 export default function RootLayout({
-    children,
+  children,
 }: {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }) {
-    return (
-        <html lang="en">
-            <body>
-                <MenuProvider>
-                    <ContactFormProvider>
-                        <ContactForm />
-                        <Toaster />
-                        <div className="flex">
-                            <SideBar />
-                            <main className="flex-1 pl-0 lg:pl-[300px]">
-                                <Navbar />
-                                {children}
-                                <Analytics />
-                            </main>
-                        </div>
-                    </ContactFormProvider>
-                </MenuProvider>
-            </body>
-        </html>
-    );
+  return (
+    <html lang="en">
+      <body>
+        <MenuProvider>
+          <ContactFormProvider>
+            <ContactForm />
+            <Toaster />
+            <div className="flex">
+              <SideBar />
+              <main className="flex-1 pl-0 lg:pl-[300px]">
+                <Navbar />
+                {children}
+                <Analytics />
+              </main>
+            </div>
+          </ContactFormProvider>
+        </MenuProvider>
+      </body>
+    </html>
+  );
 }
