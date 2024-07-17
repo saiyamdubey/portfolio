2d30b7d515c41605e73ed1cb7c89c95dd1f49bb4
Saiyam Dubey <saiyamdubey8787@gmail.com>
2024-07-18 02:00:33 +0530

    Removed Some email realted bugs

--- src/app/api/email/route.ts
+++ src/app/api/email/route.ts
@@ -1,12 +1,12 @@
 import { NextResponse } from 'next/server';
 import { Resend } from 'resend';
-const resend = new Resend(process.env.RESEND_KEY);
+const resend = new Resend("saiyamdubey8787@gmail.com");
 
 export async function POST(req: Request) {
     const { name, email, message } = await req.json();
     await resend.sendEmail({
         from: 'Acme <onboarding@resend.dev>',
-        to: 'nwalia.ez@gmail.com',
+        to: 'saiyamdubey01@gmail.com',
         subject: 'hello world',
         text: JSON.stringify({ name, email, message }),
     });
